e1263631027595d324393e9b96522e6556345df4
liangzx <zhixuliang@yunify.com>
2022-05-16 13:24:21 +0800

    docs: mdf readme.md

--- samples/helloworld.go
+++ samples/helloworld.go
@@ -1,74 +1,82 @@
 package main
 
 import (
-    "context"
-    "encoding/json"
-    "fmt"
-    "log"
-    "math/rand"
-    "time"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
 
-    paho "github.com/eclipse/paho.mqtt.golang"
+	"github.com/pkg/errors"
 
-    "github.com/tkeel-io/device-sdk-go/client"
-    "github.com/tkeel-io/device-sdk-go/util/wait"
+	paho "github.com/eclipse/paho.mqtt.golang"
+
+	"github.com/tkeel-io/device-sdk-go/client"
+	"github.com/tkeel-io/device-sdk-go/util/wait"
 )
 
 const (
-    _brokerAddr = "tcp://139.198.112.150:1883"
-    _username   = "iotd-ddbbba2c-0f35-4943-abf3-ab789a68f864"
-    _pwd        = "NmFlODBlYzQtMjdiMi0zMzM0LTkyMTMtMTU2NmI5NGFmOWVh"
+	_brokerAddr = "tcp://139.198.112.150:1883"
+	_username   = "iotd-ddbbba2c-0f35-4943-abf3-ab789a68f864"
+	_pwd        = "NmFlODBlYzQtMjdiMi0zMzM0LTkyMTMtMTU2NmI5NGFmOWVh"
 )
 
 func main() {
-    log.SetFlags(log.Lshortfile)
+	log.SetFlags(log.Lshortfile)
 
-    cli := client.NewClient(_brokerAddr, _username, _pwd)()
+	cli := client.NewClient(_brokerAddr, _username, _pwd)()
 
-    err := cli.Connect()
-    if err != nil {
-        log.Fatalln(err)
-    }
+	err := cli.Connect()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    cli.SubscribeRaw(context.TODO(), rawTopicHandler)
-    cli.SubscribeAttribute(context.TODO(), attributesTopicHandler)
-    cli.SubscribeCommand(context.TODO(), commandsTopicHandler)
+	_ = cli.SubscribeRaw(context.TODO(), rawTopicHandler)
+	_ = cli.SubscribeAttribute(context.TODO(), attributesTopicHandler)
+	_ = cli.SubscribeCommand(context.TODO(), commandsTopicHandler)
 
-    tm := time.Second * 1
+	tm := time.Second * 1
 
-    wait.EveryWithContext(context.Background(), func(ctx context.Context) {
-        v, _ := deviceValue()
-        // telemetry
-        cli.PublishTelemetry(ctx, v)
-    }, tm)
+	wait.EveryWithContext(context.Background(), func(ctx context.Context) {
+		v, _ := deviceValue()
+		// telemetry.
+		_ = cli.PublishTelemetry(ctx, v)
+	}, tm)
 
-    select {}
+	select {}
 }
 
 func attributesTopicHandler(cli paho.Client, message paho.Message) {
-    fmt.Printf("attributes=%s\n", string(message.Payload()))
+	fmt.Printf("attributes=%s\n", string(message.Payload()))
 }
 
 //
 func commandsTopicHandler(cli paho.Client, message paho.Message) {
-    fmt.Printf("commands=%s\n", string(message.Payload()))
+	fmt.Printf("commands=%s\n", string(message.Payload()))
 }
 
 //
 func rawTopicHandler(cli paho.Client, message paho.Message) {
-    fmt.Printf("rawTopic=%s\n", string(message.Payload()))
+	fmt.Printf("rawTopic=%s\n", string(message.Payload()))
 }
 
 func deviceValue() ([]byte, error) {
-    mv := map[string]interface{}{
-        "humidity":      rand.Intn(20),
-        "temperature":   rand.Intn(20),
-        "pressure":      1015.3,
-        "windDirection": 21,
-        "windSpeed":     1.2,
-        "latitude":      29.10,
-        "longtidude":    107.05,
-        "voltage":       5.1,
-    }
-    return json.Marshal(mv)
+	mv := map[string]interface{}{
+		"humidity":      rand.Intn(20),
+		"temperature":   rand.Intn(20),
+		"pressure":      1015.3,
+		"windDirection": 21,
+		"windSpeed":     1.2,
+		"latitude":      29.10,
+		"longitude":     107.05,
+		"voltage":       5.1,
+	}
+
+	bys, err := json.Marshal(mv)
+	if err != nil {
+		err = errors.Wrap(err, "deviceValue")
+	}
+
+	return bys, err
 }
